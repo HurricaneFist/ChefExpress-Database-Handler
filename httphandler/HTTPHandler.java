ef675cb0b416748d9b55bdde84e3af9023d66af2
Ian <pyropter28@gmail.com>
2015-11-30 20:16:07 -0600

    Basic database functionality is fully functional

--- httphandler/HTTPHandler.java
+++ httphandler/HTTPHandler.java
@@ -12,28 +12,55 @@ import javax.net.ssl.HttpsURLConnection;
 import java.util.ArrayList;
 import java.util.Collections;
 
-// compile with: javac -cp httphandler/json-simple-1.1.1.jar httphandler/*.java
-
 public class HTTPHandler {
 
-    // for now you'll need to manually enter these
+    // API ID and API key needed to access the API
 	final static String ID;
 	final static String KEY;
 
-    // max number of results returned
+    // The max number of results to have the API return to the user
 	final static int MAX_RESULTS = 100;
+	
+	/* Attribution requirements for using Yummly's API, can only be
+	 * used after getRecipes() has already been called
+	 */
+	static ArrayList<String> attribution = new ArrayList<String>();
 
-	// simply calling getRecipes with no specification for testing purposes
+	/**
+	 * Takes an ArrayList<String> of ingredients, and returns an ArrayList<Recipe> of recipes
+	 * which contain the ingredients provided.
+	 * @param ingredients              a list of ingredients
+	 * @return An ArrayList of Recipe objects.
+	 * @throws Exception
+	 */
 	public static ArrayList<Recipe> getRecipes(ArrayList<String> ingredients) throws Exception {
 		return getRecipes(ingredients, "", -1, -1, -1, -1, -1);
 	}
 
-    // the highly customizable version of getRecipes
+    /**
+     * Same as the above method, but allows for much more user customization for things such 
+     * as dietary restrictions, sodium limits, calories, protein, preparation time, etc.
+     * 
+     * If an empty string is entered as an argument for any String parameter, that parameter
+     * is not specified for. Likewise, if a negative number is entered as an argument for 
+     * any Integer parameter, that parameter is not specified for.
+     * 
+     * @param ingredients              a list of ingredients
+     * @param cuisine                  the type of cuisine (i.e. american, french, mediterranean, etc.)
+     * @param kCalMin                  minimum calories 
+     * @param kCalMax                  maximum calories 
+     * @param carbLimit                minimum carbohydrates in grams
+     * @param proteinMin               minimum protein in grams
+     * @param timeLimit                maximum time to prepare recipe in seconds
+     * @return
+     * @throws Exception
+     */
 	public static ArrayList<Recipe> getRecipes(ArrayList<String> ingredients, String cuisine, int kCalMin, int kCalMax, int carbLimit, int proteinMin,
 	                                           int timeLimit) throws Exception {
 
-		boolean cuisineSpecified = cuisine.equals("") ? false : true; // if an empty string is entered as an argument for cuisine, then cuisine is unspecified
-		boolean kCalMinSpecified = (kCalMin < 0) ? false : true; // if integer parameters for mins/maxs are inputted as negative, they are unspecified
+		// Check if each parameter was specified for by the user
+		boolean cuisineSpecified = cuisine.equals("") ? false : true; 
+		boolean kCalMinSpecified = (kCalMin < 0) ? false : true; 
 		boolean kCalMaxSpecified = (kCalMax < 0) ? false : true;
 		boolean carbLimitSpecified = (carbLimit < 0) ? false : true;
 		boolean proteinMinSpecified = (proteinMin < 0) ? false : true;
@@ -42,92 +69,137 @@ public class HTTPHandler {
 		if (kCalMinSpecified && kCalMaxSpecified && kCalMin > kCalMax)
 			throw new Exception("ERROR: Minimum calories specified cannot be greater than maximum calories specified.");
 
-        // initializing the ArrayList of recipes to be returned
+        // Initializing the ArrayList of recipes to be returned
 		ArrayList<Recipe> recipes = new ArrayList<Recipe>();
 
-        // parameters for the GET call
+        // Processing parameters for the URL used in the GET call
 		String params = "";
 		for (int i = 0; i < ingredients.size(); i++)
-		  // the ingredients argument is an arraylist of ingredients, i.e. new ArrayList(Arrays.asList("cinnamon", "apple", "butter"));
 			params += ("&allowedIngredient[]=" + ingredients.get(i));
 
-	    // kCalMin is an integer in kilocalories (AKA food calories)
 		if (kCalMinSpecified)
 			params += "&nutrition.ENERC_KCAL.min=" + kCalMin;
    
-        // kCalMax is same as above
 		if (kCalMaxSpecified)
 			params += "&nutrition.ENERC_KCAL.max=" + kCalMax;
 
-		// the cuisine argument should be a demonym like "american", "french", "mediterranean", etc.
 		if (cuisineSpecified)
 			params += "&allowedCuisine[]=cuisine^cuisine-" + cuisine;
 
-		// the carbLimit argument should be an integer in grams
 		if (carbLimitSpecified)
 			params += "&nutrition.CHOCDF.max=" + carbLimit;
 
-		// the proteinMin argument should be an integer in grams
 		if (proteinMinSpecified)
 			params += "&nutrition.PROCNT.min=" + proteinMin;
 
-        // a limit on how long the meal should take to prepare, in seconds
 		if (timeLimitSpecified)
 			params += "&maxTotalTimeInSeconds=" + timeLimit;
 
+		// Initializing the HTTP URL connection
 		URL url = new URL("http://api.yummly.com/v1/api/recipes?_app_id=" + ID + "&_app_key=" + KEY + params + "&maxResult=" + MAX_RESULTS);
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
 		conn.setRequestMethod("GET");
 
+		// Acquiring the JSON from the GET call
 		BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-
 		StringBuffer responseJSON = new StringBuffer(in.readLine());
 
+		// Parsing the JSON returned from the GET call
 		JSONParser parser = new JSONParser();
 		JSONObject json = (JSONObject) parser.parse(responseJSON.toString());
 		JSONArray data = (JSONArray) json.get("matches");
+		
+		// Get the attribution
+		JSONObject rawAttribution = (JSONObject) json.get("attribution");
+		attribution.add((String) rawAttribution.get("html"));
+		attribution.add((String) rawAttribution.get("url"));
+		attribution.add((String) rawAttribution.get("text"));
+		attribution.add((String) rawAttribution.get("logo"));
 
+		/* Looping through each recipe, converting it to a Recipe object, and adding it to the 
+		 * ArrayList<Recipe> which will be returned by getRecipes
+		 */
 		for (int i = 0; i < data.size(); i++) {
 			JSONObject jsonRecipe = (JSONObject) data.get(i);
 			JSONArray jsonIngredients = (JSONArray) jsonRecipe.get("ingredients");
+			
 			ArrayList<String> returnedIngredients = new ArrayList<String>();
-
 			for (int j = 0; j < jsonIngredients.size(); j++)
-				returnedIngredients.add((String) jsonIngredients.get(j)); // load the ingredients from the JSON obj into the returned obj
+				returnedIngredients.add((String) jsonIngredients.get(j)); 
 
-			// the more ingredients the recipe has that the user didn't enter, the lower the relevance
+			// Relevance of a recipe is inversely proportional to the number of ingredients it contains
 			int relevance = -returnedIngredients.size();
 
-            // getting the data for the returned recipe object from the JSON, TODO: test it!
+			// Set the time needed to prepare each recipe, if the user specified timeLimit
+			long returnedTime;
+			if (jsonRecipe.get("totalTimeInSeconds") == null)
+				returnedTime = -1;
+			else
+				try {
+					returnedTime = (Long) jsonRecipe.get("totalTimeInSeconds");
+				} catch (Exception e) {
+					returnedTime = -1;
+				}
+			
+			/* Set all cuisines the recipe is in (apparently a recipe can be in multiple cuisines),
+			 * if the user specified cuisine
+			 */
+			ArrayList<String> returnedCuisines = new ArrayList<String>();
+			if (cuisineSpecified) {
+				JSONObject rawAttributes = (JSONObject) jsonRecipe.get("attributes");
+				JSONArray rawCuisines = (JSONArray) rawAttributes.get("cuisine");
+				for (int j = 0; j < rawCuisines.size(); j++)
+					returnedCuisines.add((String) rawCuisines.get(j));
+			}
+			
+			// Check if image URL exists, if it doesn't then put some filler there for it
+			String imageURL = "NO IMAGE URL FOUND";
+			if ((jsonRecipe.get("smallImageUrls")) != null)
+				imageURL = (String) ((JSONArray) jsonRecipe.get("smallImageUrls")).get(0);
+			
+			// Create a new Recipe object for each recipe in the array, and add that to the ArrayList<Recipe>
 			recipes.add(new Recipe(
 				(String) jsonRecipe.get("recipeName"),
-				(String) jsonRecipe.get("smallImageUrls"),
+				imageURL,
 				"FILLER INSTRUCTIONS",  // TODO: find a method of getting instructions for the recipe, if it exists
 				returnedIngredients,
-				Integer.parseInt((String) jsonRecipe.get("totalTimeInSeconds")),
-				(String) ((JSONObject) jsonRecipe.get("attributes")).get("cuisine"),
+				returnedTime,
+				returnedCuisines,
 				relevance
 			));
 		}
 
 		in.close();
-		Collections.sort(recipes); // TODO: check if sorting works properly
+		
+		// Sort the ArrayList<Recipe> based on relevance, and return it
+		Collections.sort(recipes); 
 		return recipes;
 	}
 
 	public static void main(String[] args) throws Exception {
 
-		/*
-		Test code
+		/* USAGE INSTRUCTIONS
+		
+		// Example input: Search for recipes containing cinnamon and apple.
+		
+		ArrayList<String> input = new ArrayList<String>() {{
+			add("cinnamon");
+			add("apple");
+		}};
+		
+		// Output: Returns a list of 100 recipes containing cinnamon and apple in them
+		// The first (most relevant) result returned is applesauce, which can be made with only cinnamon and apple
+		// The last (least relevant) result returned may be roasted turkey, which needs many more ingredients than provided
+		
+		ArrayList<Recipe> list = getRecipes(input);
 
-		ArrayList<String> input = new ArrayList<String>();
-		input.add("cinnamon");
-		input.add("apple");
-
-		System.out.println(getRecipes(input));
-
+		for (Recipe recipe: list)
+			System.out.println(recipe);
+		
+		// After calling getRecipes(), it is required to display the attribution to Yummly's API
+		
+		System.out.println(attribution);
+		
 		*/
 	}
-
 }
--- httphandler/Recipe.java
+++ httphandler/Recipe.java
@@ -4,22 +4,33 @@ import java.util.*;
 
 public class Recipe implements Comparable<Recipe> {
 
-	public String cuisine, name, imageURL, instructions;
-	public int cookingTime, relevance;
-	public ArrayList<String> ingredients;
+	// Name of the recipe, a link to an image of it, and the instructions for it
+	public String name, imageURL, instructions;
 
-	public Recipe(String name, String imageURL, String instructions, ArrayList<String> ingredients, int cookingTime, String cuisine, int relevance) {
+    // Relevance is a property of each recipe which determines its position in the search results
+	public int relevance;
+	
+	// Approximately how long a recipe takes to prepare, in seconds
+	public long cookingTime;
+	
+	// The ingredients for a recipe
+	public ArrayList<String> ingredients, 
+	
+	// Each cuisine a recipe belongs to
+	cuisines;
+
+	public Recipe(String name, String imageURL, String instructions, ArrayList<String> ingredients, long cookingTime, ArrayList<String> cuisines, int relevance) {
 		this.name = name;
 		this.imageURL = imageURL;
 		this.instructions = instructions;
 		this.ingredients = ingredients;
 		this.cookingTime = cookingTime;
-		this.cuisine = cuisine;
+		this.cuisines = cuisines;
 		this.relevance = relevance;
 	}
 
 	public int compareTo(Recipe other) { 
-		return relevance - other.relevance;
+		return other.relevance - relevance;
 	}
 
 	public String toString() {
